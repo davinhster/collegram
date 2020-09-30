21da442ec4f56eca6d191850bfbf4e6cec570472
Angelo Ibarrola <vino_1105@hotmail.com>
2020-09-29 18:43:53 -0700

    added signup

--- src/components/pages/signup.jsx
+++ src/components/pages/signup.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 
 function Signup(){
+
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    
+
+    handleSubmit = (evt) =>{
+        event.preventDefault();
+        const user = {
+            email: this.state.email,
+            password: this.state.password
+        }
+    }
+
+    handleChange = (evt) => {
+        event.preventDefault();
+        const {name, value} = event.target;
+    }
+
     return(
         <section>
             <div className="Signup">
-                <h1> Signup</h1>
+                <form>
+                    <label> Email </label>
+                    <input type="email" name="email" onChange={this.handleChange} />
+
+                    <label> Password </label>
+                    <input type="password" name="password" onChange={this.handleChange}/>
+                </form>
             </div>
         </section>
     )

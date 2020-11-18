489972ef6cb0a253ece63ee2488c4064f59b3416
Berlun Devera <berlun.devera@sjsu.edu>
2020-11-17 19:16:06 -0800

    Landing page aesthetic tweaks

--- src/components/pages/landing.jsx
+++ src/components/pages/landing.jsx
@@ -9,6 +9,8 @@ function Landing(){
             <div className="landing1">
                 <h1 className="landingHeader"> Collegram </h1>
                 <h2 className="landingSubheader"> The best college social network! </h2>
+                <div className="divider"/>
+                <h3 className="landingDescription"> Collegram is a social network where you can connect with friends and classmates. Need to reach out to one of your classmates to finish their part of a project? Collegram offers a chat option so you can chat with them in real time! What are you waiting for? Register now! </h3>
                 <Link to="/signin">
                     <button className="loginBtn"> Login </button>
                 </Link>

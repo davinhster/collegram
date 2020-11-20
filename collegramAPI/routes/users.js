5f20b13519e73aa454e2f7a9cb249990a8ad3be6
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2020-11-20 10:14:11 -0800

    Replaced bcrypt with bcrypt-nodejs module

--- collegramAPI/routes/users.js
+++ collegramAPI/routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/user.model');
-const bcript = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
 const jwt = require('jsonwebtoken');
 const mongoosoe = require('mongoose');
 

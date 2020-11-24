3e24d6b434163428465d20b9b65062cb0962234d
Arash Bahramian <arash.bahramiand@gmail.com>
2020-11-24 12:44:35 -0800

    changing navbar and button colors
    making the navbar and buttons match the new background image.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,27 +1,36 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
-
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">Collegram</Link>
+      <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
+        <Link to="/" className="navbar-brand">
+          Collegram
+        </Link>
         <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/home" className="nav-link">Home</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/signin" className="nav-link">Login</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/signup" className="nav-link">Register</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/about" className="nav-link">About</Link>
-          </li>
-        </ul>
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/home" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/signin" className="nav-link">
+                Login
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/signup" className="nav-link">
+                Register
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/about" className="nav-link">
+                About
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     );
